1267d345bae509609015805f5ba04e444e001584
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-01-30 21:17:41 +0900

    aaa

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-
-  process resize_to_fit: [800, 800]
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
-  
-  
+  storage :file
+  # storage :fog
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_29_024914) do
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birthday", null: false
+    t.string "birthday"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
