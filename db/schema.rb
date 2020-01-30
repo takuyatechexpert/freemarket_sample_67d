25037af1403ccba1aa2db82cc205cb74909475e3
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-01-30 12:18:59 +0900

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_01_29_024914) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "nick_name", null: false
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birth_year", null: false
-    t.string "birth_month", null: false
-    t.string "birth_day", null: false
+    t.string "birthday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -30,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_01_29_024914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name"
+    t.index ["nick_name"], name: "index_users_on_nick_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
