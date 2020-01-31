2756af4a6785941934e026aec61faeb089d00a48
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-01-31 12:58:46 +0900

    fix exhibitionpage
    fix exhibitionpage css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
          has_many :cards
          has_many :addresses
 
-         validates :nickname,              presence: true, length: {maximum: 20}
+         validates :nick_name,              presence: true, length: {maximum: 20}
          validates :email,                 presence: true, uniqueness: { case_sensitive: false }
          validates :password,              presence: true, length: {minimum: 7, maximum: 128}
          validates :family_name,           presence: true, length: {maximum: 35}
          validates :first_name,             presence: true, length: {maximum: 35}
          validates :family_name_kana,      presence: true, length: {maximum: 35}
          validates :first_name_kana,       presence: true, length: {maximum: 35}
-         validates :birthday,              presence: true
+        #  validates :birthday,              presence: true
 end
