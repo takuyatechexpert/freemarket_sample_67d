53b3f1978f209bb7aac71dd319b3af265238a230
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-02-09 01:51:36 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_020257) do
+ActiveRecord::Schema.define(version: 2020_02_08_103550) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
     t.string "municipalities", null: false
     t.string "address", null: false
     t.string "home_type"
-    t.integer "tel", null: false
+    t.bigint "tel", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -27,10 +27,17 @@ ActiveRecord::Schema.define(version: 2020_02_03_020257) do
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "coustomer_id", null: false
     t.string "card_id", null: false
-    t.bigint "user_id", null: false
+    t.string "references", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -43,17 +50,18 @@ ActiveRecord::Schema.define(version: 2020_02_03_020257) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.text "description", null: false
     t.integer "price", null: false
+    t.text "description", null: false
     t.string "size", null: false
-    t.integer "status_id", null: false
-    t.integer "postage_id", null: false
-    t.integer "region_id", null: false
-    t.integer "shipping_date_id", null: false
+    t.integer "item_status", null: false
+    t.integer "delivery_charge", null: false
+    t.string "delivery_area", null: false
+    t.integer "shipment", null: false
     t.integer "buyer_id"
     t.integer "seller_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -76,6 +84,5 @@ ActiveRecord::Schema.define(version: 2020_02_03_020257) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "cards", "users"
   add_foreign_key "images", "items"
 end
