5e787f23776665b79b4b9bd6c69f85b667914196
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-02-08 18:01:57 +0900

    fix file
    turbolinksの追加
    不要なimageファイルの削除
    出品機能マージ後の調整

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :item, optional: true
 
-  validates :name, presence: true
+  validates :image, presence: true
 
   mount_uploader :image, ImageUploader
 end
