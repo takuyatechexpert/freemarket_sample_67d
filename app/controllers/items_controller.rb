25c83ca432bb56510d1d3b7cccbdbd21534c093d
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-02-12 14:32:29 +0900

    fix error handling
    削除失敗時のerror handlingの記述追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,8 +50,11 @@ class ItemsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:id])
-      @item.destroy
-      redirect_to root_path
+      if@item.destroy
+        redirect_to root_path
+      else
+        redirect_to item_path
+      end
   end
 
   private
