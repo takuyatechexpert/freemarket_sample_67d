5819d36085e66c9110837fa16e2cb53c60a78bbe
ikedakenji <59217589+ikedakenji@users.noreply.github.com>
2020-02-13 17:11:14 +0900

    登録商品の表示
    DBから商品の画像を持ってきて
    ・toppage
    ・userpage
    にそれぞれ表示をさせた。

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,8 @@ class ToppagesController < ApplicationController
   
   def index
     @category = Category.all.order("id ASC").limit(9)
-    
+    @item = Item.all.order("id ASC").last(6)
+    @image = Image.all.order("id ASC").last(6)
   end
 
 end
