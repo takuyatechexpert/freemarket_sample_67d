c5276947746d3b9f32a8192cfec19a3040652433
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-02-13 19:41:16 +0900

    ancestry

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,18 +8,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new
-
-    @category = Category.all.order("id ASC").limit(9)
+    @category = Category.all.order("id ASC").limit(13)
   end
 
-  def category_children
-    @category_children = Category.find(params[:productCategory]).children
+
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
 
-  # def category_grandchildren
-  #   @category_grandchildren = Category.find(params[:productcategory]).children
-  # end
+ def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+ end
 
+ 
   def create
     @user = User.find(current_user.id)
     @item = Item.new(item_params)
@@ -27,8 +28,9 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
+      @category = Category.all.order("id ASC").limit(13)
       @item.images.new
-      redirect_to new_item_path
+      render :new
     end
   end
 
