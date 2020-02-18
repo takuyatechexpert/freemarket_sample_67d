a9f25f7d728f1fd4dc0b2a67b9d856205bd5a7df
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-02-18 13:57:03 +0900

    fix edit category
    編集ページでカテゴリーデータの保持
    編集を可能に変更

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -30,14 +30,14 @@ $(function(){
     var parentCategory = $(this).val();
     if (parentCategory != "---"){ //親カテゴリーが初期値でないことを確認
       $.ajax({
-        url: 'get_category_children',
+        url: '/items/get_category_children',
         type: 'GET',
         data: { parent_name: parentCategory },
         dataType: 'json'
       })
       .done(function(children){
         $('#child_category').remove(); //親が変更された時、子以下を削除するする
-        $('#grandchildren_wrapper').remove();
+        $('#grand_child_category').remove();
         var insertHTML = '';
         children.forEach(function(child){
           insertHTML += appendOption(child);
@@ -58,7 +58,7 @@ $(function(){
     var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
     if (childId != "---"){ //子カテゴリーが初期値でないことを確認
       $.ajax({
-        url: 'get_category_grandchildren',
+        url: '/items/get_category_grandchildren',
         type: 'GET',
         data: { child_id: childId },
         dataType: 'json'
@@ -66,8 +66,6 @@ $(function(){
       .done(function(grandchildren){
         if (grandchildren.length != 0) {
           $('#grand_child_category').remove(); //子が変更された時、孫以下を削除するする
-          $('#size_wrapper').remove();
-          $('#brand_wrapper').remove();
           var insertHTML = '';
           grandchildren.forEach(function(grandchild){
             insertHTML += appendOption(grandchild);
